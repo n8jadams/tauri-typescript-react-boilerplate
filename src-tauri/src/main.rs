307d69b95d0bd3393d00042faaabe35b6b21ada5
Nate Adams <nateisneato@gmail.com>
2021-06-06 06:56:35 -0600

    Seemingly fixes things in Windows. Personally having problems with the WebView2Loader.dll...

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,4 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+mod menu;
 
 #[derive(serde::Serialize)]
 struct CustomResponse {
@@ -19,6 +16,7 @@ async fn message_from_rust(window: tauri::Window) -> Result<CustomResponse, Stri
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![message_from_rust])
+    .menu(menu::get_menu())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
